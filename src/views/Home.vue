66743653246eb517c92acc58cbd4394971d8b73d
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2022-01-12 22:05:35 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <RandomBox/>
+  </div>
+</template>
+
+<script>
+import RandomBox from '../components/RandomBox.vue';
+
+export default {
+    components : {RandomBox}
+}
+</script>
+
+<style>
+
+</style>
